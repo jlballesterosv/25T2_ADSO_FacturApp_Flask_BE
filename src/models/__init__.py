a3d6e81d6437986d1ded09b09e2ce96fa7237883
Jorge Ballesteros <joba1021@netcracker.com>
2026-05-26 18:31:22 -0500

    Inicial conexion bd

---
+++ src/models/__init__.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import pymysql
+
+engine = create_engine("mysql+pymysql://root@localhost:3306/FacturApp_25T2_PY?charset=utf8mb4")
+
+connection = engine.connect()
+
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+Base = declarative_base()
+Base.metadata.bind = engine
